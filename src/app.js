1545bab58302d11cd5531f2f0ce18a3fd347c357
precious654 <odukwechiemeka@gmail.com>
2024-10-10 16:59:43 +0100

    added body parser and handled CORS errors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use("/products", productRoutes);
 app.use("/orders", ordersRoutes);
 
-app.use((rea, res, next) => {
+app.use((req, res, next) => {
     const error = new Error("Not Found");
     error.status = 404;
     next(error);
